6955e7ac8159432f3b8cdbd0a66dfb5adefa50ca
Brian Faust <hello@basecode.sh>
2019-07-15 07:51:27 +0300

    refactor: drop concurrently

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,41 +1,43 @@
-'use strict';
+"use strict";
 
-import gulp from 'gulp'
-import babel from 'gulp-babel'
-import less from 'gulp-less'
-import watch from 'gulp-watch'
+import gulp from "gulp";
+import babel from "gulp-babel";
+import less from "gulp-less";
+import watch from "gulp-watch";
 import * as gulpBrowser from "@pushrocks/gulp-browser";
-import jade from 'gulp-jade'
+import jade from "gulp-jade";
 
-gulp.task('babel', () => {
+gulp.task("babel", () => {
   gulp
-    .src(['src/**/*.js', '!src/wallet.js'])
-    .pipe(watch('src/**/*.js', { verbose: true }))
-    .pipe(babel({ presets: ['@babel/preset-env'] }))
-    .pipe(gulp.dest('lib'))
-})
+    .src(["src/**/*.js", "!src/wallet.js"])
+    .pipe(watch("src/**/*.js", { verbose: true }))
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(gulp.dest("lib"));
+});
 
-gulp.task('less', () => {
+gulp.task("less", () => {
   gulp
-    .src('src/**/*.less')
-    .pipe(watch('src/**/*.less'))
+    .src("src/**/*.less")
+    .pipe(watch("src/**/*.less"))
     .pipe(less())
-    .pipe(gulp.dest('lib'))
-})
+    .pipe(gulp.dest("lib"));
+});
 
-gulp.task('jade', () => {
+gulp.task("jade", () => {
   gulp
-    .src('src/**/*.jade')
-    .pipe(watch('src/**/*.jade'))
+    .src("src/**/*.jade")
+    .pipe(watch("src/**/*.jade"))
     .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('.'))
-})
+    .pipe(gulp.dest("."));
+});
 
-gulp.task('bundle', () => {
+gulp.task("bundle", () => {
   gulp
-    .src('src/wallet.js')
-    .pipe(watch('src/wallet.js', { verbose: true }))
-    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .src("src/wallet.js")
+    .pipe(watch("src/wallet.js", { verbose: true }))
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
     .pipe(gulpBrowser.browserify())
-    .pipe(gulp.dest('lib'))
-})
+    .pipe(gulp.dest("lib"));
+});
+
+gulp.task("default", gulp.parallel("babel", "less", "jade", "bundle"));
--- src/wallet.js
+++ src/wallet.js
@@ -1,37 +1,35 @@
-import crypto from 'crypto'
-import * as bip39 from 'bip39'
-import arkjs from 'arkjs'
+import crypto from "crypto";
+import * as bip39 from "bip39";
+import arkjs from "arkjs";
 
-angular.module('wallet', [])
-  .factory('wallet', () => {
+angular.module("wallet", []).factory("wallet", () => {
+  return {
+    mnemonicToData: passphrase => {
+      if (!passphrase) {
+        passphrase = bip39.generateMnemonic();
+      }
 
-    return {
-      mnemonicToData: (passphrase) => {
-        if (!passphrase) {
-          passphrase = bip39.generateMnemonic()
-        }
+      let networks = arkjs.networks;
+      let ecpair = arkjs.ECPair.fromSeed(passphrase, networks.ark);
 
-        let networks = arkjs.networks
-        let ecpair = arkjs.ECPair.fromSeed(passphrase, networks.ark)
+      let publicKey = ecpair.getPublicKeyBuffer().toString("hex");
+      let address = ecpair.getAddress().toString("hex");
+      let wif = ecpair.toWIF();
 
-        let publicKey = ecpair.getPublicKeyBuffer().toString('hex')
-        let address = ecpair.getAddress().toString('hex')
-        let wif = ecpair.toWIF()
-
-        return {
-          passphrase,
-          passphraseqr: '{"passphrase":"'+passphrase+'"}',
-          address: address,
-          addressqr: '{"a":"'+address+'"}',
-          publicKey: publicKey,
-          wif: wif,
-          entropy: bip39.mnemonicToEntropy(passphrase),
-        }
-      },
-      validateMnemonic: (mnemonic) => {
-        return bip39.validateMnemonic(mnemonic)
-      },
-      randomBytes: crypto.randomBytes,
-      entropyToMnemonic: bip39.entropyToMnemonic
-    }
-  })
+      return {
+        passphrase,
+        passphraseqr: '{"passphrase":"' + passphrase + '"}',
+        address: address,
+        addressqr: '{"a":"' + address + '"}',
+        publicKey: publicKey,
+        wif: wif,
+        entropy: bip39.mnemonicToEntropy(passphrase)
+      };
+    },
+    validateMnemonic: mnemonic => {
+      return bip39.validateMnemonic(mnemonic);
+    },
+    randomBytes: crypto.randomBytes,
+    entropyToMnemonic: bip39.entropyToMnemonic
+  };
+});
