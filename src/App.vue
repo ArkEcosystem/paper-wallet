cbc59125ea67351b2dd10d249e296dff901db900
ItsANameToo <itsanametoo@protonmail.com>
2019-07-23 17:05:55 +0200

    chore: proper footer urls

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,12 @@
             <div class="flex flex-col text-gray-500 text-center mt-10 mb-5">
                 <span>
                     Hosted by GitHub Pages. Source code is available
-                    <a class="inline-link" href target="_blank">here</a>. You can download and run locally
-                    <a class="inline-link" href target="_blank">here</a>
+                    <a class="inline-link" href="https://github.com/ArkEcosystem/paper-wallet" target="_blank">here</a>. You can download and run locally
+                    <a class="inline-link" href="https://github.com/ArkEcosystem/paper-wallet/releases" target="_blank">here</a>
                 </span>
                 <span class="text-sm mt-2">
                     Made with ❤️
-                    <a class="inline-link" href target="_blank">ARK.io</a>
+                    <a class="inline-link" href="https://ark.io" target="_blank">ARK.io</a>
                 </span>
             </div>
         </div>
