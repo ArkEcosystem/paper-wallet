678f67c6f70240abe2776040f89eef19778743f8
Brian Faust <hello@basecode.sh>
2020-05-10 10:39:44 +0300

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -3,22 +3,5 @@ const autoprefixer = require("autoprefixer");
 const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-    plugins: [
-        tailwindcss("./tailwind.config.js"),
-        autoprefixer(),
-        purgecss({
-            content: ["./src/**/*.vue"],
-            extractors: [
-                {
-                    extractor: class TailwindExtractor {
-                        static extract(content) {
-                            return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-                        }
-                    },
-                    extensions: ["html", "vue"],
-                },
-            ],
-            whitelist: ["html", "body"],
-        }),
-    ],
+    plugins: [tailwindcss("./tailwind.config.js"), autoprefixer()],
 };
--- src/message.ts
+++ src/message.ts
@@ -2,27 +2,22 @@ import secp256k1 from "secp256k1";
 import { sync } from "simple-sha256";
 import { Buffer } from "buffer/";
 
-// NOTE: secp256k1 expects a node.js Buffer but the polyfill has all methods we need so the @ts-ignore let us run it
 export const signMessage = (message: string, passphrase: string) => {
     const privateKey = Buffer.from(sync(passphrase), "hex");
 
     return {
-        // @ts-ignore
-        publicKey: secp256k1.publicKeyCreate(privateKey).toString("hex"),
-        signature: secp256k1
-            // @ts-ignore
-            .signatureExport(secp256k1.sign(Buffer.from(sync(message), "hex"), privateKey).signature)
-            .toString("hex"),
+        publicKey: Buffer.from(secp256k1.publicKeyCreate(privateKey)).toString("hex"),
+        signature: Buffer.from(
+            secp256k1.signatureExport(secp256k1.ecdsaSign(Buffer.from(sync(message), "hex"), privateKey).signature),
+        ).toString("hex"),
         message,
     };
 };
 
 export const verifyMessage = ({ message, publicKey, signature }) => {
-    return secp256k1.verify(
-        // @ts-ignore
+    return secp256k1.ecdsaVerify(
+        secp256k1.signatureImport(Uint8Array.from(Buffer.from(signature, "hex"))),
         Buffer.from(sync(message), "hex"),
-        // @ts-ignore
-        secp256k1.signatureImport(Buffer.from(signature, "hex")),
         Buffer.from(publicKey, "hex"),
     );
 };
